18dd8fd571042db6905fa27f30d8dfd038362a49
gloriaadanna <gloriaagwam7@gmail.com>
2023-03-23 04:25:13 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h> 
 
 /**
- * main - prints the number from 1 to 
+ * main - prints the number from 1 to 100
  * Return: always 0
  */
 int main(void)
@@ -17,7 +17,7 @@ int main(void)
 		} else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz");
-		} else if ( i % 3 == 0 && i % 5 == 0)
+		} else if ( i % 3 == 0 && i % 5 != 0)
 		{
 			printf("FizzBuzz");
 		} else if (i == 1)
