e765ec50161f8ad52b5c01d1fe4b97e4ca7fed4e
gloriaadanna <gloriaagwam7@gmail.com>
2023-04-03 09:05:26 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] >= '\0'; i++)
+	int i = 0;
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return (s + 1);
+			return (&s[i]);
 	}
-	return (NULL);
+	return (0);
 }
