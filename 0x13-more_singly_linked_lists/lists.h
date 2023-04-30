2fc8e6b155bbaacc0cb979a9ad315a89ed24d4be
derhieakpos <derhieakpos@gmail.com>
2023-04-30 07:42:10 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
 #include <stdio.h>
-
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-	listint_t;
-}
+#include <stdlib.h>
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
