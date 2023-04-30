e6d6549bacfc3ca79154c814ae36706cf27aee42
derhieakpos <derhieakpos@gmail.com>
2023-04-30 06:07:00 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - main function of the program
+ * which prints all element of a linked list
+ * @h: type of linked list to print
+ *
+ * Return: the number of nodes when successful
+ */
+
+size_t print_listint(const listint_t *h);
+{
+	size_t num = 0;
+	while (h)
+	{
+		printf("%d\n", h-f);
+		num++;
+		h = h->forward;
+	}
+	return (num);
+}
