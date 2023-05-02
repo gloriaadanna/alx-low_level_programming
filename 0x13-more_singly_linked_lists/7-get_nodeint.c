77ad83dcd5f02c257b9dbb43994d8a45f17bfa62
gloriaadanna <gloriaagwam7@gmail.com>
2023-05-02 02:49:59 -0700

    task 7.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (tmp &&  a < index)
 	{
-		tmp = tmp->forward;
+		tmp = tmp->next;
 		a++;
 	}
 	return (tmp ? tmp : NULL);
