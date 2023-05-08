0462dd827e5182cc1eb82a100dfd0970d12a6f47
gloriaadanna <gloriaagwam7@gmail.com>
2023-05-08 07:12:01 -0700

    task 4.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,4 +35,47 @@ void close_file(int f)
 		exit(100);
 	}
 }
+/**
+ * main - main function
+ * @argc: arguments
+ * @argv: pointers
+ * Return: 0 when successful
+ */
+int main(int argc, char *argv[])
+{
+	int fro, be, t, a;
+	char *buffer;
 
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buffer = create_buffer(argv[2]);
+	fro = open(argv[1], O_RDONLY);
+	t = read(fro, buffer, 1024);
+	be = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do if (fro == -1 || t == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: cannot read from file %s\n", argv[1]);
+		free(buffer);
+		exit(98);
+	}
+
+	a = write(be, buffer, t);
+	if (be == -1 || a == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: cannot write to %s\n", argv[2]);
+		free(buffer);
+		exit(99);
+	}
+	t = read(fro, buffer, 1024);
+	be = open(argv[2], O_WRONLY | O_APPEND);
+	while (t > 0);
+
+	free(buffer);
+	close_file(fro);
+	close_file(be);
+	return (0);
+}
