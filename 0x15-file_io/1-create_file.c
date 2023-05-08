669148abf9d4e5410b7a29cf0132a46bd4420d1a
gloriaadanna <gloriaagwam7@gmail.com>
2023-05-08 06:12:05 -0700

    task 1.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *create_file - the main function of the program that 
+ * create_file - the main function of the program that 
  * creates a file
  * @filename: the file to be created
  * @text_content: the content written in the file
@@ -8,6 +8,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int letters
-		int w;
+	int fd, b, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	b = write(fd, text_content, len);
+
+	if (fd == -1 || b == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
